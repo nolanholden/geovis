1d3138592a651ab4c702c004572c8e4d5efb01f4
Nolan Holden <nolanholden@users.noreply.github.com>
2017-06-01 22:44:29 -0400

    Minor adjustments

--- payload/payload.ino
+++ payload/payload.ino
@@ -1,5 +1,5 @@
 #include <Wire.h>
-#include "Adafruit_BMP085.h"
+#include <Adafruit_BMP085.h>
 
 /* 
  * BMP180 setup instructions:
@@ -11,56 +11,61 @@
  * ----------------------------------------
 */
 
-Adafruit_BMP085 barometer;
+Adafruit_BMP085 bmp;
   
 void setup() {
-  Serial.begin(9600); // 9600 bits/second
-  if (!barometer.begin()) {
-  	Serial.println("No BMP085 sensor found.");
+  Serial.begin(2000000); // bits/second
+  if (!bmp.begin()) {
+  	Serial.println("No BMP085 sensor found. Program will not proceed.");
   	while (1) { /* Trap the thread. */ }
   }
 }
   
 void printBaroData() {
 
-    // Get temperature.
-    Serial.print("Temperature = ");
-    Serial.print(barometer.readTemperature());
-    Serial.println(" *C");
+  // Get temperature.
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
 
-    // Get pressure at sensor.
-    Serial.print("Pressure = ");
-    Serial.print(barometer.readPressure());
-    Serial.println(" Pa");
+  // Get pressure at sensor.
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
 
-    // Calculate pressure at 0 MSL. (0 meters mean sea-level)
-    Serial.print("Calculated pressure at 0 MSL = ");
-    Serial.print(barometer.readSealevelPressure());
-    Serial.println(" Pa");
+  // Calculate pressure at 0 MSL. (0 meters mean sea-level)
+  Serial.print("Calculated pressure at 0 MSL = ");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.println(" Pa");
 
-    // Get pressure altitude:
-    // altitude with (default) altimeter setting at 101325 Pascals == 1013.25 millibars
-    // == 29.92 inches mercury (i.e., std. pressure)
-    Serial.print("Pressure altitude = ");
-    Serial.print(barometer.readAltitude());
-    Serial.println(" meters");
+  // Get pressure altitude:
+  // altitude with (default) altimeter setting at 101325 Pascals == 1013.25 millibars
+  // == 29.92 inches mercury (i.e., std. pressure)
+  Serial.print("Pressure altitude = ");
+  Serial.print(bmp.readAltitude());
+  Serial.println(" meters");
 
-    // TODO:
-    // Density altitude:
-    // pressure altitude corrected for nonstandard temperature
-    // High density altitude (High, Hot, and Humid) means decreased performance.
+  // TODO:
+  // Density altitude:
+  // pressure altitude corrected for nonstandard temperature
+  // High density altitude (High, Hot, and Humid) means decreased performance.
 
-    // Get indicated altitude:
-    // pressure altitude corrected for non-standard pressure, with altimeter 
-    // setting 1015 millibars == 101500 Pascals
-    // For pressure conversions, visit NOAA at: https://www.weather.gov/media/epz/wxcalc/pressureConversion.pdf.
-    Serial.print("Indicated altitude = ");
-    Serial.print(barometer.readAltitude(101500));
-    Serial.println(" meters");
+  // Get indicated altitude:
+  // pressure altitude corrected for non-standard pressure, with 
+  // altimeter setting (in Pascals) as argument 
+  // (29.92 inches mercury == 101325 Pascals == 1013.25 millibars)
+  // For pressure conversions, visit NOAA at: https://www.weather.gov/media/epz/wxcalc/pressureConversion.pdf.
+  Serial.print("Indicated altitude =                                  ");
+  Serial.print(bmp.readAltitude());
+  Serial.println(" meters");
+  Serial.print("Indicated altitude with sensor pressure adjustment =  ");
+  Serial.print(bmp.readAltitude((float)bmp.readPressure()));
+  Serial.println(" meters");
 
-    Serial.println();
+  Serial.println();
 }
+
 void loop() {
-    printBaroData();
-    delay(350);
+  printBaroData();
+  delay(200);
 }
