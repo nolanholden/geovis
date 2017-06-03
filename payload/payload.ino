fa6a84cf4921864a358888e40a74fdd99b9b4892
Nolan Holden <nolanholden@users.noreply.github.com>
2017-06-02 20:41:22 -0400

    Outsource the BMP180 logic

--- payload/payload.ino
+++ payload/payload.ino
@@ -26,48 +26,9 @@ Adafruit_BMP085 bmp;
 void setup() {
   // Start serial communication.
   Serial.begin(9600); // in bits/second
-
-  // Start the BMP-180. Trap the thread if no sensor is found.
-  if (!bmp.begin()) {
-  	Serial.println("No BMP sensor found. Program will not proceed.");
-  	while (1) { /* Trap the thread. */ }
-  }
 }
 
 void writeBmpData() {}
-
-struct BmpData {
-  
-  // Temperature.
-  // // UNIT: degrees Celcius
-  float temperature;
-
-  // Pressure at the sensor.
-  // UNIT: pascal (N/m^2)
-  int32_t ambient_pressure;
-  
-  // Pressure altitude: altitude with altimeter setting at 101325 Pascals == 1013.25 millibars
-  // == 29.92 inches mercury (i.e., std. pressure) // For pressure conversions, visit NOAA 
-  // at: https://www.weather.gov/media/epz/wxcalc/pressureConversion.pdf.
-  // (Pressure altitude is NOT correct for non-standard pressure or temperature.)
-  // UNIT: meters
-  float pressure_altitude;
-
-  // Only if a humidity sensor is viable, TODO (Nolan Holden):
-  // Add density altitude:
-  // pressure altitude corrected for nonstandard temperature.
-  // Remember: higher density altitude (High, Hot, and Humid) means decreased performance.
-
-  BmpData(float _temperature, int32_t _ambient_pressure, float _pressure_altitude) {
-    temperature = _temperature;
-    ambient_pressure = _ambient_pressure;
-    pressure_altitude = _pressure_altitude;
-  }
-
-  // Disallow copying and moving.
-  BmpData(const BmpData&) = delete;
-  BmpData& operator=(const BmpData&) = delete;
-};
 
 BmpData& getBmpData(void) {
   return BmpData(bmp.readTemperature(), bmp.readPressure(), bmp.readAltitude());
