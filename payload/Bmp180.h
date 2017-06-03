de3c64e26ebd118771a85932b964a52acd7c7287
Nolan Holden <nolanholden@users.noreply.github.com>
2017-06-02 20:19:52 -0400

    Add header for Bmp custom type

---
+++ payload/Bmp180.h
@@ -0,0 +1,15 @@
+#ifndef RCR_LEVEL1PAYLOAD_BMP180_H_
+#define RCR_LEVEL1PAYLOAD_BMP180_H_
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+namespace rcr {
+namespace level1payload {
+} // namespace level1_payload
+} // namespace rcr
+
+#endif // RCR_LEVEL1PAYLOAD_BMP180_H_
