9fd3c8f055b774cad2b23dce77a0ccb0192c9a0f
Nolan Holden <nolanholden@users.noreply.github.com>
2017-06-13 19:55:09 -0400

    Put code into encapsulating src dir

---
+++ src/payload/Debug/payload.cpp.d
@@ -0,0 +1,87 @@
+C:\Users\John-lenovo\AppData\Local\Temp\VMBuilds\payload\teensy36\Debug\payload.cpp.o: \
+ C:\Users\John-lenovo\AppData\Local\Temp\VMBuilds\payload\teensy36\Debug\payload.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/WProgram.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/avr/pgmspace.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/avr/interrupt.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/avr_functions.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/wiring.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/core_id.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/core_pins.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/kinetis.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/avr/pgmspace.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/new.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/avr_emulation.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/usb_serial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/usb_desc.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/usb_seremu.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/usb_keyboard.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/keylayouts.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/usb_mouse.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/usb_joystick.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/usb_midi.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/usb_rawhid.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/usb_flightsim.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/usb_mtp.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/usb_audio.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/usb_touch.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/usb_undef.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/elapsedMillis.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/IntervalTimer.h \
+ C:\Users\John-lenovo\AppData\Local\Temp\VMBuilds\payload\teensy36\Debug\payload.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\Adafruit_Unified_Sensor/Adafruit_Sensor.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\cores\teensy3/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\libraries\Wire/Wire.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\libraries\Wire/WireKinetis.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\teensy\avr\libraries\SPI/SPI.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/BlockDriver.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/BaseBlockDriver.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/FatLibConfig.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/SdFatConfig.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/SdCard/SdSpiCard.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/SysCall.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/SdCard/SdInfo.h \
+ c:\users\john-lenovo\documents\arduino\libraries\sdfat\src\fatlib\baseblockdriver.h \
+ c:\users\john-lenovo\documents\arduino\libraries\sdfat\src\spidriver\sdspidriver.h \
+ c:\users\john-lenovo\documents\arduino\libraries\sdfat\src\spidriver\SdSpiBaseDriver.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FreeStack.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/MinimumSerial.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/SysCall.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/SdFat.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/BlockDriver.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/FatLib.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/ArduinoFiles.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/FatFile.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/FatApiConstants.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/FatStructs.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/FatVolume.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/ArduinoStream.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/bufstream.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/iostream.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/istream.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/ios.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/ostream.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/FatFileSystem.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/StdioStream.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/FatLib/fstream.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\SdFat\src/SdCard/SdioCard.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\Adafruit_BME280_Library/Adafruit_BME280.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\Adafruit_GPS_Library/Adafruit_GPS.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\Adafruit_BNO055/Adafruit_BNO055.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\Adafruit_BNO055/utility/imumaths.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\Adafruit_BNO055/utility/vector.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\Adafruit_BNO055/utility/matrix.h \
+ C:\Users\John-lenovo\Documents\Arduino\libraries\Adafruit_BNO055/utility/quaternion.h \
+ C:\Users\John-lenovo\AppData\Local\Temp\VMBuilds\payload\teensy36\Debug\setupable.h \
+ C:\Users\John-lenovo\AppData\Local\Temp\VMBuilds\payload\teensy36\Debug\setup-object.h \
+ C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2017\Community\Common7\IDE\Extensions\02oghilb.30b\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h
