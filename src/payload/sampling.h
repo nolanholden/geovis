99c14f982ca81aa22fe012f6c246868a0bcbf75a
Nolan Holden <nolanholden@users.noreply.github.com>
2017-06-20 19:40:57 -0400

    Add header for IMU vector sampling

---
+++ src/payload/sampling.h
@@ -0,0 +1,18 @@
+#ifndef RCR_LEVEL1PAYLOAD_SAMPLING_H_
+#define RCR_LEVEL1PAYLOAD_SAMPLING_H_
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+namespace rcr {
+namespace level1payload {
+
+
+
+} // namespace level1_payload
+} // namespace rcr
+
+#endif // RCR_LEVEL1PAYLOAD_SAMPLING_H_
