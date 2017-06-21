5a73b47c4bab682332bfe54888f484e961ae8951
Nolan Holden <nolanholden@users.noreply.github.com>
2017-06-21 16:58:50 -0400

    update string implementation

--- src/payload/data-acquisition-interface.h
+++ src/payload/data-acquisition-interface.h
@@ -7,6 +7,8 @@
 	#include "WProgram.h"
 #endif
 
+#include "sampling.h" // Inertial Measurement Unit vector sampling utilities
+
 namespace rcr {
 namespace level1payload {
 
@@ -26,25 +28,41 @@ static void append_barometric_data(Adafruit_BME280& bme, String& string_to_appen
 
   // Pressure altitude (meters)
   string_to_append += bme.readAltitude(1013.25f); // 101325 Pa (std pressure)
+  string_to_append += ",";
 }
 
 // 9DOF data (inertial & orientation) from BNO055.
 static void append_inertial_data(Adafruit_BNO055& bno, String& string_to_append) {
-  std::stringstream stream; // TODO: test computational cost of various string implementations here.
+  String line = ""; // TODO: test computational cost of various string implementations here.
 
-  // Orientation "vector"
+  //// Orientation "vector"
   auto euler = sample_imu<3>(bno, Adafruit_BNO055::VECTOR_EULER);
-  stream << euler.x() << "," << euler.y() << "," << euler.z() << ",";
+  line += euler.x();
+  line += ",";
+  line += euler.y();
+  line += ",";
+  line += euler.z();
+  line += ",";
 
   // Linear acceleration vector
   auto linear = sample_imu<3>(bno, Adafruit_BNO055::VECTOR_LINEARACCEL);
-  stream << linear.x() << "," << linear.y() << "," << linear.z() << ",";
+  line += euler.x();
+  line += ",";
+  line += euler.y();
+  line += ",";
+  line += euler.z();
+  line += ",";
 
   // Gravitational accelleration vector
   auto gravity = sample_imu<3>(bno, Adafruit_BNO055::VECTOR_GRAVITY);
-  stream << gravity.x() << "," << gravity.y() << "," << gravity.z() << ",";
+  line += euler.x();
+  line += ",";
+  line += euler.y();
+  line += ",";
+  line += euler.z();
+  line += ",";
 
-  string_to_append += stream.str().c_str();
+  string_to_append += line;
 }
 
 } // namespace level1_payload
