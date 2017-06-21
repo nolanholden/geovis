059b50797f71c260241cfd88c093d53dad010a05
Nolan Holden <nolanholden@users.noreply.github.com>
2017-06-20 20:14:35 -0400

    Organizational changes

--- src/payload/payload.h
+++ src/payload/payload.h
@@ -7,6 +7,9 @@
 	#include "WProgram.h"
 #endif
 
+#include <string>
+#include <vector>
+
 // Sensor libraries.
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
@@ -19,20 +22,26 @@
 #include <SdFat.h>
 #include <SdFatConfig.h>
 #include <SysCall.h>
-#include <FreeStack.h> // something is wrong with this library.
+//#include <FreeStack.h> // something is wrong with this library.
 
 //#include <lib/i2c_t3/i2c_t3.h> // I2C for teensy (replaces wire.h)
 
 // RCR headers
 #include "setup-object.h"
+#include "sampling.h" // Inertial Measurement Unit vector sampling utilities
 
 namespace rcr {
 namespace level1payload {
 
+// TODO: setup runtime filename resolution. (likely use millis())
+// File path names
 static constexpr const char* kGpsLogPath = "gps.log";
-static constexpr const char* kOrientationLogPath = "9dof.log";
+static constexpr const char* kOrientationLogPath = "imu.log";
 static constexpr const char* kBarometricLogPath = "baro.log";
-static constexpr const uint32_t kLoopDelay = 1024; // milliseconds
+
+// Main loop delay (milliseconds)
+static constexpr const uint32_t kLoopDelay = 1024;
+
 
 // File I/O
 File file; // File manager
@@ -56,38 +65,13 @@ inline void print_with_ellipses(const char* message) {
   delay(512);
 }
 
-inline void setup_objects() {
-  setup_object<SdFatSdio>(sd_card, "SD card");
-  setup_object<Adafruit_GPS>(gps, uint32_t{9600}, "GPS sensor");
-  setup_object<Adafruit_BNO055>(bno, "BNO055");
-  setup_object<Adafruit_BME280>(bme, "BME280");
-}
-
-inline void setup() {
-  // Illuminate LED.
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-  delay(3000);
-  
-  // Start serial communication. 
-  Serial.begin(9600); // bits/second does not matter for Teensy 3.6
-  print_with_ellipses("In setup");
-
-  // Setup objects / verify working condition.
-  setup_objects();
-
-  Serial.println();
-  Serial.println("SETUP COMPLETE.");
-  Serial.println();
-} // setup()
-
 void write_to_sd(const char* path, const String& content) {
   // Open a (new/existing) file for writing.
   file = sd_card.open(path, FILE_WRITE);
 
-  // Initiate file for writing.
-  if(!file) {
-    Serial.println("File could not be initialized."); 
+  // Write to file (if able).
+  if (!file) {
+    Serial.println("File could not be initialized.");
     // Swallow the error.
   }
   else {
@@ -96,7 +80,16 @@ void write_to_sd(const char* path, const String& content) {
   }
 }
 
-void append_bme_data(String& string_to_append) {
+// Setup objects / verify working condition.
+inline void setup_objects() {
+  setup_object<SdFatSdio>(sd_card, "SD card");
+  setup_object<Adafruit_GPS>(gps, uint32_t{9600}, "GPS sensor");
+  setup_object<Adafruit_BNO055>(bno, "BNO055");
+  setup_object<Adafruit_BME280>(bme, "BME280");
+}
+
+// Barometric data (altitude) from BME280.
+void append_barometric_data(String& string_to_append) {
   // Temperature (*C)
   string_to_append += bme.readTemperature();
   string_to_append += ",";
@@ -113,14 +106,42 @@ void append_bme_data(String& string_to_append) {
   string_to_append += bme.readAltitude(1013.25f); // 101325 Pa (std pressure)
 }
 
-void append_bno_data(String& string_to_append) {
+// 9DOF data (inertial / orientation) from BNO055.
+void append_inertial_data(String& string_to_append) {
+
+  // Linear acceleration vector.
+  auto linear = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+  
+  // Gravitational accelleration vector.
+  auto gravity = bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY);
 }
+
+inline void setup() {
+  // Illuminate LED.
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  delay(3000);
+
+  // Start serial communication. 
+  Serial.begin(9600); // bits/second does not matter for Teensy 3.6
+  print_with_ellipses("In setup");
+
+  setup_objects();
+
+  // Initialize output files.
+  Serial.println("Setting up output files...");
+  write_to_sd(kBarometricLogPath, "*C, Pa, %, m");
+  Serial.println("SUCCESS: files ready.");
+
+  Serial.println();
+  Serial.println("SETUP COMPLETE.");
+  print_with_ellipses("Starting loop");
+} // setup()
 
 String bme_data = "";
 inline void loop() {
   bme_data = "";
-  write_to_sd(kBarometricLogPath, "*C, Pa, %, m");
-  append_bme_data(bme_data);
+  append_barometric_data(bme_data);
   write_to_sd(kBarometricLogPath, bme_data);
   delay(kLoopDelay);
 }
@@ -144,7 +165,6 @@ void printBmeData() {
   Serial.println(" meters");
   Serial.println();
 }
-
 
 } // namespace level1_payload
 } // namespace rcr
