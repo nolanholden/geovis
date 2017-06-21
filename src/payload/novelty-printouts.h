7191be867de7f236e44e5c969287444205e238d6
Nolan Holden <nolanholden@users.noreply.github.com>
2017-06-21 15:03:52 -0400

    Remove extra println

--- src/payload/novelty-printouts.h
+++ src/payload/novelty-printouts.h
@@ -19,29 +19,28 @@ inline void print_with_ellipses(const char* message) {
   Serial.print(".");
   delay(512);
   Serial.println(".");
-  Serial.println();
   delay(512);
 }
 
 // PROVISIONAL. REMOVE BEFORE FLIGHT. =^.^=
-void printBmeData() {
-  Serial.print("Temperature = ");
-  Serial.print(bme.readTemperature());
-  Serial.println(" *C");
-
-  Serial.print("Ambient pressure = ");
-  Serial.print(bme.readPressure());
-  Serial.println(" Pa");
-
-  Serial.print("Relative humidity = ");
-  Serial.print(bme.readHumidity());
-  Serial.println(" percent");
-
-  Serial.print("Pressure altitude = ");
-  Serial.print(bme.readAltitude(1013.25f)); // 101325 Pa (i.e., std pressure)
-  Serial.println(" meters");
-  Serial.println();
-}
+//void printBmeData(Adafruit_BME280& bme) {
+//  Serial.print("Temperature = ");
+//  Serial.print(bme.readTemperature());
+//  Serial.println(" *C");
+//
+//  Serial.print("Ambient pressure = ");
+//  Serial.print(bme.readPressure());
+//  Serial.println(" Pa");
+//
+//  Serial.print("Relative humidity = ");
+//  Serial.print(bme.readHumidity());
+//  Serial.println(" percent");
+//
+//  Serial.print("Pressure altitude = ");
+//  Serial.print(bme.readAltitude(1013.25f)); // 101325 Pa (i.e., std pressure)
+//  Serial.println(" meters");
+//  Serial.println();
+//}
 
 } // namespace level1_payload
 } // namespace rcr
--- src/payload/setup-object.h
+++ src/payload/setup-object.h
@@ -41,7 +41,6 @@ inline void setup_object(T& obj, const char* display_name) {
     Serial.print(display_name);
     Serial.println(" ready.");
   }
-  Serial.println();
 }
 
 } // namespace level1_payload
