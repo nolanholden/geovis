1665e1b9020fe135c959922e871a2f2d83f3fb0d
Nolan Holden <nolanholden@users.noreply.github.com>
2017-07-01 16:20:01 -0400

    add uart header

---
+++ src/payload/uart.cpp
@@ -0,0 +1,55 @@
+#include "uart.h"
+
+SERCOM sercom1;
+//Uart Serial1;
+
+Uart::Uart() {
+  bufferReadPosition = 0;
+  bufferWritePosition = 0;
+  echo = 0;
+}
+
+Uart::Uart(SERCOM *sercom, int a, int b, int c, int d) {}
+void Uart::begin(int pin) {}
+
+void Uart::write(char c) {
+  if(echo == 1) {
+    putchar(c);
+  }
+}
+
+int Uart::available() {
+  return (bufferWritePosition - bufferReadPosition > 0);
+}
+
+//char Uart::read() {
+//  char c = buffer[bufferReadPosition];
+//  bufferReadPosition++;
+//
+//  if(bufferReadPosition >= BUFFER) {
+//    bufferReadPosition = 0;
+//  }
+//
+//  return c;
+//}
+
+void Uart::insert(const char *buffer) {
+  for(int i=0; buffer[i] != '\0'; i++) {
+    this->buffer[bufferWritePosition] = buffer[i];
+    bufferWritePosition++;
+
+    if(bufferWritePosition >= BUFFER) {
+      bufferWritePosition = 0;
+    }
+  }
+}
+
+void Uart::IrqHandler() {}
+
+void Uart::enableEcho() {
+  echo = 1;
+}
+
+void Uart::disableEcho() {
+  echo = 0;
+}
---
+++ src/payload/uart.h
@@ -0,0 +1,39 @@
+#ifndef UART_H
+#define UART_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include "HardwareSerial.h"
+
+#define PIO_SERCOM 0
+#define SERCOM_RX_PAD_0 0
+#define UART_TX_PAD_2 0
+
+#define BUFFER 1024
+
+class SERCOM {};
+
+class Uart : public HardwareSerial {
+  public:
+    Uart();
+    Uart(SERCOM *sercom, int a, int b, int c, int d);
+    void begin(int pin);
+    void write(char c);
+    int available();
+    //char read() final;
+    void insert(const char *buffer);
+    void IrqHandler();
+    void enableEcho();
+    void disableEcho();
+  private:
+    char buffer[BUFFER];
+    int bufferReadPosition;
+    int bufferWritePosition;
+    int echo;
+};
+
+extern SERCOM sercom1;
+//extern Uart Serial1;
+
+#endif
