ff4dd0030f336928dd4e29bd84ab4f1c6a32b4b0
Nolan Holden <nolanholden@users.noreply.github.com>
2017-07-02 02:25:31 -0400

    rid of unnecessary typedef

--- src/payload/kalman.h
+++ src/payload/kalman.h
@@ -4,13 +4,13 @@
 namespace rcr {
 namespace level1payload {
 
-typedef struct {
+struct kalman_t {
   float process_noise;     // process noise covariance
   float measurement_noise; // measurement noise covariance
   float value;             // value
   float error;             // estimation error covariance
   float gain;              // kalman gain
-} kalman_t;
+};
 
 } // namespace level1_payload
 } // namespace rcr
