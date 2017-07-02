3b7d42684ba05fbff4064f2c72219e70fcf19677
Nolan Holden <nolanholden@users.noreply.github.com>
2017-07-02 00:50:12 -0400

    rid of old code

--- src/payload/novelty-printouts.h
+++ src/payload/novelty-printouts.h
@@ -23,26 +23,6 @@ inline void print_with_ellipses(T message) {
   delay(512);
 }
 
-// PROVISIONAL. REMOVE BEFORE FLIGHT. =^.^=
-//void printBmeData(Adafruit_BME280& bme) {
-//  Serial.print("Temperature = ");
-//  Serial.print(bme.readTemperature());
-//  Serial.println(" *C");
-//
-//  Serial.print("Ambient pressure = ");
-//  Serial.print(bme.readPressure());
-//  Serial.println(" Pa");
-//
-//  Serial.print("Relative humidity = ");
-//  Serial.print(bme.readHumidity());
-//  Serial.println(" percent");
-//
-//  Serial.print("Pressure altitude = ");
-//  Serial.print(bme.readAltitude(1013.25f)); // 101325 Pa (i.e., std pressure)
-//  Serial.println(" meters");
-//  Serial.println();
-//}
-
 } // namespace level1_payload
 } // namespace rcr
 
