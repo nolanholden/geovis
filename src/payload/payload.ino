a8993368240954ed6f530eabba473f9e64c2b28c
Nolan Holden <nolanholden@users.noreply.github.com>
2017-07-06 13:15:51 -0400

    remove no-longer-needed includes

--- src/payload/payload.ino
+++ src/payload/payload.ino
@@ -1,20 +1,5 @@
 #include "payload.h" // C++ file which acts as a surrogate this .ino program.
 
-// Must include libraries here for those using the Arduino environment.
-// Sensors:
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-#include <Adafruit_BNO055.h>
-
-// SD I/O:
-#include <BlockDriver.h>
-#include <MinimumSerial.h>
-#include <SdFat.h>
-#include <SdFatConfig.h>
-#include <SysCall.h>
-//#include <FreeStack.h> // something is wrong with this library.
-
-
 void setup() {
   rcr::level1payload::setup();
 }
