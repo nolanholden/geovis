b654d4b7acd971222d1da42ad8a523c6b993bf50
Nolan Holden <nolanholden@users.noreply.github.com>
2017-07-10 00:34:02 -0400

    add logfile name

--- src/geovis/constants.h
+++ src/geovis/constants.h
@@ -9,6 +9,9 @@ constexpr uint8_t kLedPin = (uint8_t)13;
 // Main loop delay (milliseconds)
 constexpr const unsigned long kLoopDelay = 256ul;
 
+// Log path
+constexpr const char* kLogPath = "flight.csv";
+
 // Kalman constants:
 constexpr float KALMAN_PROCESS_NOISE = 0.01f;
 constexpr float KALMAN_MEASUREMENT_NOISE = 0.25f;
